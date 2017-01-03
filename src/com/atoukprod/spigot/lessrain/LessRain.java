2371e802a72cd50ae4b78c61d602143b334220e9
rgaborieau <romain.gaborieau@hotmail.fr>
2017-01-03 15:17:50 +0100

    Add scoreboard settings

--- src/com/atoukprod/spigot/lessrain/LessRain.java
+++ src/com/atoukprod/spigot/lessrain/LessRain.java
@@ -6,6 +6,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
 
 import java.util.Random;
 
@@ -13,6 +16,10 @@ import java.util.Random;
  * Created by Romain on 28/12/2016.
  */
 public class LessRain extends JavaPlugin implements Listener{
+
+    private Scoreboard board;
+    private Objective settings;
+
     @Override
     public void onDisable() {
         super.onDisable();
@@ -24,21 +31,41 @@ public class LessRain extends JavaPlugin implements Listener{
 
         getServer().getPluginManager().registerEvents(this, this);
 
+        board = Bukkit.getScoreboardManager().getMainScoreboard();
+
+        settings = board.getObjective("pluginlessrain");
+
+        if(settings==null)
+        {
+            settings = board.registerNewObjective("pluginlessrain","dummy");
+            settings.getScore("verbose").setScore(0);
+            settings.getScore("numerator").setScore(0);
+            settings.getScore("denominator").setScore(1);
+        }
+
     }
 
     @EventHandler
     public void onRain(WeatherChangeEvent event)
     {
-        Bukkit.broadcastMessage("Oh, it's raining today !");
-        int r = (new Random()).nextInt(2);
-        if(r==0)
+
+        int verbose = settings.getScore("verbose").getScore();
+        int numerator = settings.getScore("numerator").getScore();
+        int denominator = settings.getScore("denominator").getScore();
+
+
+
+        if(verbose==1)Bukkit.broadcastMessage("Oh, it's raining today !");
+        
+        float r = (new Random()).nextFloat();
+        if(r<numerator/denominator)
         {
-            Bukkit.broadcastMessage("Chance, this time the rain is gone !");
+            if(verbose==1)Bukkit.broadcastMessage("Chance, this time the rain is gone !");
             event.setCancelled(true);
         }
         else
         {
-            Bukkit.broadcastMessage("Oh shit, luck isn't with me this time");
+            if(verbose==1)Bukkit.broadcastMessage("Oh shit, luck isn't with me this time");
         }
 
     }
